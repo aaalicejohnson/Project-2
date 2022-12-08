16d5a616e03d73528a4cfe1940edcaaffedbb3df
Alice Johnson <aaalicejohnson@gmail.com>
2022-12-07 20:26:24 -0500

    pushing API data to sql

--- query.sql
+++ query.sql
@@ -21,5 +21,21 @@ release_year INT,
 rating VARCHAR
 );
 
-select * from netflix
-select * from disney_plus
+CREATE TABLE all_movies(
+id INT, 
+title TEXT PRIMARY KEY,
+director TEXT, 
+movie_cast TEXT, 
+country TEXT, 
+release_year INT, 
+rating VARCHAR,
+budget FLOAT, 
+revenue FLOAT, 
+runtime FLOAT, 
+movie_id INT, 
+genre TEXT, 
+streaming VARCHAR);
+
+select * from netflix;
+select * from disney_plus;
+select * from all_movies;
